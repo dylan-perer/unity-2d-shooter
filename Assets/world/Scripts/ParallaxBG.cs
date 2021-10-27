08239911e18026533dacd729e986d44c895ab634
Aiden <aiden.p2908@gmail.com>
2021-10-27 14:40:06 +1300

    parallax background 1st pass

---
+++ Assets/world/Scripts/ParallaxBG.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBG : MonoBehaviour
+{
+    //
+    [SerializeField] private Vector2 parallaxEffectMutliplier;
+    private Transform cammeraTransform;
+    private Vector3 lastCammeraPostion;
+    private void Start()
+    {   
+        cammeraTransform = Camera.main.transform;
+        lastCammeraPostion = cammeraTransform.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = cammeraTransform.position - lastCammeraPostion;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMutliplier.x, deltaMovement.y * parallaxEffectMutliplier.y);
+        lastCammeraPostion = cammeraTransform.position;
+    }
+}
