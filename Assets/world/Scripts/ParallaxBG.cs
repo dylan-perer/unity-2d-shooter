21334b8858ba42e527413d5f515f6fe065899651
Aiden <aiden.p2908@gmail.com>
2021-10-28 00:18:11 +1300

    repeating parallax + new jumping animation

--- Assets/world/Scripts/ParallaxBG.cs
+++ Assets/world/Scripts/ParallaxBG.cs
@@ -12,12 +12,19 @@ public class ParallaxBG : MonoBehaviour
     {   
         cammeraTransform = Camera.main.transform;
         lastCammeraPostion = cammeraTransform.transform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
     }
 
     private void LateUpdate()
     {
         Vector3 deltaMovement = cammeraTransform.position - lastCammeraPostion;
-        transform.position += new Vector3(deltaMovement.x * parallaxEffectMutliplier.x, deltaMovement.y * parallaxEffectMutliplier.y);
+        transform.position += new Vector3(-1 *(deltaMovement.x * parallaxEffectMutliplier.x), deltaMovement.y * parallaxEffectMutliplier.y);
         lastCammeraPostion = cammeraTransform.position;
+
+        // if(Mathf.Abs(cammeraTransform.position.x - transform.position.x) >= textureUnitSizeX){
+        //     // float offsetPositionX = (cammeraTransform.position.x - transform.position.x) % textureUnitSizeX;
+        //     transform.position = new Vector3(cammeraTransform.position.x, transform.position.y);
+        // }
     }
 }
